9b93b5703424f9fd9e61c20cdb3e5058f0d92ce7
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-11-01 22:02:23 +0600

    Add project files.

---
+++ HasanInventoryManagementSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HasanInventoryManagementSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
