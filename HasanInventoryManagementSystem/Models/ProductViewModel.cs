2fd56d226de0ba880d41e50ec981a91c1f918541
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-11-04 20:42:07 +0600

    Add Product view model

---
+++ HasanInventoryManagementSystem/Models/ProductViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace HasanInventoryManagementSystem.Models
+{
+    public class ProductViewModel
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public string ProductBrand { get; set; }
+        public string ProductCategory { get; set; }
+        public decimal Price { get; set; }
+    }
+}
