9f140b7a4beb2cf31a117cb9541db528323f2570
amdadulhaquehasan <amdadul.hasan75757@gmail.com>
2025-11-04 23:00:52 +0600

    Update Product View Model Class

--- HasanInventoryManagementSystem/Models/ProductViewModel.cs
+++ HasanInventoryManagementSystem/Models/ProductViewModel.cs
@@ -2,10 +2,13 @@
 {
     public class ProductViewModel
     {
+        #region Properties
         public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string ProductBrand { get; set; }
         public string ProductCategory { get; set; }
         public decimal Price { get; set; }
+
+        #endregion
     }
 }
